b7c4db3f86e843ed9d4f917c342903d8f61f54d6
Ifechukwu Edet <ifechiglory@gmail.com>
2025-08-22 20:43:28 +0100

    bug fixes: responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TodoList from "./components/TodoList";
 export default function App() {
   return (
     <section className="flex flex-col bg-yellow-200 min-h-screen">
-      <main className="flex-grow p-6">
+      <main className="flex flex-col justify-center items-center flex-grow p-6">
         <h2 className="font-bold text-center underline uppercase mb-4 p-4 text-3xl">
           To-dos
         </h2>
